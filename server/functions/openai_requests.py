80f47be13228f955b9ebe8949743ff0f0dc363a4
Ace <kronicrespawn@gmail.com>
2023-06-15 10:08:13 -0500

    added request function: audio to text

---
+++ server/functions/openai_requests.py
@@ -0,0 +1,18 @@
+import openai
+from decouple import config
+
+
+# retrieve env variables
+openai.organization = config("OPEN_AI_ORG")
+openai.api_key = config("OPEN_AI_KEY")
+
+#Open AI - Whisper
+#Converts audio to text per documentation at openai
+def convert_audio_to_text(audio_file):
+    try:
+        transcript = openai.Audio.transcribe("whisper-1", audio_file)
+        message_text = transcript["text"]
+        return message_text 
+    except Exception as e:
+        print(e)
+        return
