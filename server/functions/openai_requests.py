548583610d60b7c0dce91bd17c3a5e6be247ec7c
Ace <kronicrespawn@gmail.com>
2023-06-19 23:59:48 -0500

    get response message

--- server/functions/openai_requests.py
+++ server/functions/openai_requests.py
@@ -1,6 +1,8 @@
 import openai
 from decouple import config
 
+#import custom functions
+from functions.database import get_recent_messages
 
 # retrieve env variables
 openai.organization = config("OPEN_AI_ORG")
@@ -16,3 +18,27 @@ def convert_audio_to_text(audio_file):
     except Exception as e:
         print(f"Error occurred during transcription: {str(e)}")
         return
+    
+
+#ai chat 
+#get response to our message
+def get_chat_response(message_input):
+    
+    messages = get_recent_messages()
+
+    user_message = {"role": "user", "content": message_input}
+    messages.append(user_message)
+
+    print(messages)
+
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages = messages
+        )
+        print(response)
+        message_text = response["choices"][0]["message"]["content"]
+        return message_text
+    except Exception as e:
+        print(e)
+        return "Done"
