b1b585b87f1521653f0dc53d8546d5ee3ebe9e92
Ace <kronicrespawn@gmail.com>
2023-06-21 11:55:24 -0500

    style updated

--- client/src/components/Title.tsx
+++ client/src/components/Title.tsx
@@ -1,44 +1,58 @@
-import {useState} from 'react';
-import axios from 'axios';
+import { useState } from "react";
+import axios from "axios";
 
-type Props = {    
-setMessages: any;
+type Props = {
+  setMessages: any;
 };
 
 function Title({ setMessages }: Props) {
-    const [isResetting, setIsResetting] = useState(false);
-    
-    // reset the conversation
-    const resetConversation = async () => {
-        setIsResetting(true);
+  const [isResetting, setIsResetting] = useState(false);
 
-        await axios.get("http://localhost:8000/reset").then((res) => {
-            if (res.status == 200) {
-                setMessages([])
-            } else {
-                console.error("there was an error with api request to backend")
-            }
-        })
-        .catch((err) => {
-            console.error(err.message);
-        })
+  // reset the conversation
+  const resetConversation = async () => {
+    setIsResetting(true);
 
-        setIsResetting(false)
-    };
+    await axios
+      .get("http://localhost:8000/reset")
+      .then((res) => {
+        if (res.status == 200) {
+            console.log(res.data);
+            setMessages([]);
+        } else {
+          console.error("there was an error with api request to backend");
+        }
+      })
+      .catch((err) => {
+        console.error(err.message);
+      });
 
-    return <div> <button onClick={resetConversation}  className="bg-indigo-500 p-5"> Reset</button> </div>
+    setIsResetting(false);
+  };
 
+  return (
+    <div className="flex justify-between items-center w-full p4 bg-gray-900 text-white font-bold shadow">
+      <div className="italic">AMARA</div>
+      <button onClick={resetConversation} 
+        className={
+            "transition-all duration-900 text-green-300 hover:text-pink-500 " + 
+        (isResetting && "animate-pulse")}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+          />
+        </svg>
+      </button>
+    </div>
+  );
 }
-
-
-// function Title() {
-    
-  
-//     return (
-//     <div>
-      
-//     </div>
-//   );
-// }
 
 export default Title;
