d7acb1831d478d549305b7edcb861d63fe8e62e9
Ace <kronicrespawn@gmail.com>
2023-06-21 12:54:24 -0500

    style updated

--- client/src/components/Controller.tsx
+++ client/src/components/Controller.tsx
@@ -1,20 +1,25 @@
-import { useState} from 'react';
-import Title from './Title';
+import { useState } from "react";
+import Title from "./Title";
+import RecordMessage from "./RecordMessage";
 
 function Controller() {
-    const [isLoading, setIsLoading] = useState(false);
-    const [messages, setMessages] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<any[]>([]);
 
-    const createBlobUrl = (data: any) => {};
+  const createBlobUrl = (data: any) => {};
 
-    const handleStop = async () => {}
-    
-  
-    return (
-      <div className='h-screen overflow-y-hidden'>
-      <Title setMessages={{setMessages}} />
+  const handleStop = async () => {};
+
+  return (
+    <div className="h-screen overflow-y-hidden">
+      <Title setMessages={{ setMessages }} />
       <div className="flex flex-col justify-between h-full overflow-y-scroll pb-96">
-        placeholder
+        {/* Recorder */}
+        <div className="fixed bottom-0 w-full py-6 border-t text-center bg-gradient-to-r from-yellow-500 to-pink-500">
+          <div className="flex justify-center item-center w-full">
+            <RecordMessage handleStop={handleStop} />
+          </div>
+        </div>
       </div>
     </div>
   );
