2a87b5c1efa78b3d85de668a8595729af8094d17
Ace <kronicrespawn@gmail.com>
2023-06-21 16:46:21 -0500

    blorb recording test successful

--- client/src/components/Controller.tsx
+++ client/src/components/Controller.tsx
@@ -5,17 +5,20 @@ import RecordMessage from "./RecordMessage";
 function Controller() {
   const [isLoading, setIsLoading] = useState(false);
   const [messages, setMessages] = useState<any[]>([]);
+  const [blob, setBlob] = useState("")
 
   const createBlobUrl = (data: any) => {};
 
   const handleStop = async (blobUrl: string) => {
     console.log(blobUrl)
+    setBlob(blobUrl)
   };
 
   return (
     <div className="h-screen overflow-y-hidden">
       <Title setMessages={{ setMessages }} />
       <div className="flex flex-col justify-between h-full overflow-y-scroll pb-96">
+        <audio src={blob} controls  />
         {/* Recorder */}
         <div className="fixed bottom-0 w-full py-6 border-t text-center bg-gradient-to-r from-yellow-500 to-pink-500">
           <div className="flex justify-center item-center w-full">
