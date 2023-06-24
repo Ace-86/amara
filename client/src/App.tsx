9055b7f1da7d5c58be852cbcbaa2bcba7bf09165
Ace <kronicrespawn@gmail.com>
2023-06-23 19:41:05 -0500

    routes added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,21 @@
 import Controller from "./components/Controller";
+import { BrowserRouter as Router, Route, Routes,  } from "react-router-dom";
+import Tasks from "./components/Tasks";
+
 
 function App() {
   return (
-    <div className="App">
-      <Controller />
-    </div>
+    <Router>
+      <div className="App">
+       
+        {/* add routes here */}
+        <Routes>
+          <Route path="/" element={<Controller />} />
+          <Route path="/tasks" element={<Tasks />} />
+          {/* Routes To Add: app menu (parent to all app components), budget, calendar */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
